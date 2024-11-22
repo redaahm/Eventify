ccdce31184a596227c78103427bc08c200d7eb57
Reda Ahmed <redamanzoor1@gmail.com>
2024-11-21 19:12:18 -0500

    Initial commit

---
+++ src/test/java/com/humber/Eventify/EventifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.Eventify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
